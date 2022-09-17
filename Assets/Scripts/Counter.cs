b3b9f3573dc89b7ff1df1faba0c06b1cd9dc1ba0
panharathsok3 <panharath.sok123@gmail.com>
2022-09-16 20:03:37 -0400

    added script
    script unity test

---
+++ Assets/Scripts/Counter.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Counter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    print("hi");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    print("loop");
+        
+    }
+}
