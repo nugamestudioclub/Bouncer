18439ce933b7d9e618d2db6a4687fcd718726358
panharathsok3 <panharath.sok123@gmail.com>
2022-09-16 20:20:04 -0400

    Update Counter.cs
    Added variables to cube

--- Assets/Scripts/Counter.cs
+++ Assets/Scripts/Counter.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 
 public class Counter : MonoBehaviour
 {
+[SerializeField]
+private GameObject cube;
+private float a = 2.00f;
+
+
     // Start is called before the first frame update
     void Start()
     {
     print("hi");
-        
+    
     }
 
     // Update is called once per frame
     void Update()
     {
-    print("loop");
-        
+    print(cube.name);
+    cube.GetComponent<Transform>().position = new Vector3(1, a, 2);
+    
+    a += 1;
+
     }
 }
