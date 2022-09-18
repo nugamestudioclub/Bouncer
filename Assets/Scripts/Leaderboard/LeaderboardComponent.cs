98cd8e7efb4acfe205e104493a6d94aa19eaf2e3
firez2469 <hampel.m@northeastern.edu>
2022-09-18 17:53:51 -0400

    saving/upload leaderboard

--- Assets/Scripts/Leaderboard/LeaderboardComponent.cs
+++ Assets/Scripts/Leaderboard/LeaderboardComponent.cs
@@ -19,7 +19,8 @@ public class LeaderboardComponent : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.score = PlayerPrefs.GetInt("score");
+        this.playerName = PlayerPrefs.GetString("name");
         LootLockerSDKManager.StartGuestSession((response) =>
         {
             if (!response.success)
