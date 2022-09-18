5bb2b34399be851133e22c8bca208ea73d87878d
BBBrassil <56983102+BBBrassil@users.noreply.github.com>
2022-09-18 18:10:39 -0400

    added dialogue GUI (#39)

--- Assets/Scripts/Dialogue/PlayerDialogueView.cs
+++ Assets/Scripts/Dialogue/PlayerDialogueView.cs
@@ -1,5 +1,19 @@
-﻿public class PlayerDialogueView : TextMessageLayoutView {
+﻿using UnityEngine;
+
+public class PlayerDialogueView : TextMessageLayoutView {
+	[SerializeField]
+	private GameObject bubblePrefab;
+
 	protected override void DoStart() {
 		base.DoStart();
+
+		// TODO: delete
+		string[] replies = new string[] {"hello", "how are you", "sorry,  I have to go"};
+		foreach( string reply in replies )
+			Add(new TextMessage(0, reply));
+	}
+
+	protected override GameObject View(TextMessage textMessage) {
+		return bubblePrefab;
 	}
 }
--- Assets/Scripts/PhoneMessengerView.cs
+++ Assets/Scripts/PhoneMessengerView.cs
@@ -1,5 +1,18 @@
-﻿public class PhoneMessengerView : TextMessageLayoutView {
+﻿using UnityEngine;
+
+public class PhoneMessengerView : TextMessageLayoutView {
+	[SerializeField]
+	GameObject[] bubblePrefabs;
+
 	protected override void DoStart() {
 		base.DoStart();
+	}
+
+	protected override GameObject View(TextMessage textMessage) {
+		return Instantiate(GetBubblePrefab(textMessage.SpeakerId), transform.parent);
+	}
+
+	private GameObject GetBubblePrefab(int id) {
+		return bubblePrefabs[id % bubblePrefabs.Length];
 	}
 }
