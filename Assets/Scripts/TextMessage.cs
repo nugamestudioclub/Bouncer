9f9725284e00bde26e38a05dd26ab91eb3d11097
BBBrassil <56983102+BBBrassil@users.noreply.github.com>
2022-09-18 13:30:37 -0400

    WIP for phone GUI which could be partially reused for DLG GUI

---
+++ Assets/Scripts/TextMessage.cs
@@ -0,0 +1,9 @@
+﻿public class TextMessage {
+	public int SpeakerId { get; private set; }
+	public string Text { get; private set; }
+
+	public TextMessage(int speakerId, string text) {
+		SpeakerId = speakerId;
+		Text = text;
+	}
+}
