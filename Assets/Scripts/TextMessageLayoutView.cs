812c39e9198e86d70f598511f961c81c6d779f70
BBBrassil <56983102+BBBrassil@users.noreply.github.com>
2022-09-18 18:04:34 -0400

    added dialogue GUI

--- Assets/Scripts/TextMessageLayoutView.cs
+++ Assets/Scripts/TextMessageLayoutView.cs
@@ -1,30 +1,17 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class TextMessageLayoutView : MonoBehaviour {
-	[SerializeField]
-	private GameObject[] textMessagePrefabs;
-
-	private IEnumerable<TextMessage> textMessageProvider;
-
+public abstract class TextMessageLayoutView : MonoBehaviour {
 	void Start() {
 		DoStart();
 	}
 
-	protected virtual void DoStart() {
-		Render();
-	}
+	protected virtual void DoStart() {	}
 
-	private IEnumerable<TextMessage> GetTempProvider() {
-		string[] texts = { "hi", "hey\n'sup?\netc", "whatever" };
-		int id = 0;
-
-		foreach( string text in texts )
-			yield return new TextMessage(this is PhoneMessengerView ? id++ % 2 : 0, text);
-	}
+	protected abstract GameObject View(TextMessage textMessage);
 
 	public void Add(TextMessage textMessage) {
-		var obj = Instantiate(GetTextMessagePrefab(textMessage.SpeakerId), transform.parent);
+		var obj = Instantiate(View(textMessage), transform.parent);
 		var view = obj.GetComponent<TextMessageView>();
 
 		view.Text = textMessage.Text;
@@ -37,18 +24,5 @@ public class TextMessageLayoutView : MonoBehaviour {
 	public void Clear() {
 		foreach( Transform child in transform )
 			Destroy(child.gameObject);
-	}
-
-	public void Render() {
-		Clear();
-
-		textMessageProvider = GetTempProvider(); /// TODO: replace
-
-		foreach( var textMessage in textMessageProvider )
-			Add(textMessage);
-	}
-
-	private GameObject GetTextMessagePrefab(int id) {
-		return textMessagePrefabs[id + textMessagePrefabs.Length % textMessagePrefabs.Length];
 	}
 }
